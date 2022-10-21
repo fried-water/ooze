db1a680596f7009ab14286c323a0129588ad04c0
Leslie Barron <lesliebarron1@gmail.com>
2022-10-21 02:57:03 -0400

    Added missing file

---
+++ src/literal.cpp
@@ -0,0 +1,53 @@
+#include "pch.h"
+
+#include "literal.h"
+
+#include <charconv>
+
+namespace ooze {
+
+namespace {
+
+template <typename T>
+std::optional<T> from_sv(std::string_view sv) {
+  T value;
+  const auto result = std::from_chars(sv.data(), sv.data() + sv.size(), value);
+  return result.ec == std::errc() ? std::optional(value) : std::nullopt;
+}
+
+} // namespace
+
+std::optional<Literal> to_literal(TokenType type, std::string_view sv) {
+  switch(type) {
+  case TokenType::Whitespace:
+  case TokenType::Comment:
+  case TokenType::Keyword:
+  case TokenType::Ident:
+  case TokenType::Symbol: return std::nullopt;
+  case TokenType::LiteralInt: {
+    const auto suffix_pos =
+      std::distance(sv.begin(), std::find_if(sv.begin(), sv.end(), [](char c) { return c == 'u' || c == 'i'; }));
+
+    const std::string_view number(sv.data(), suffix_pos);
+    const std::string_view suffix(sv.data() + suffix_pos, sv.size() - suffix_pos);
+
+    if(suffix == "i8") return Literal{*from_sv<i8>(number)};
+    if(suffix == "i16") return Literal{*from_sv<i16>(number)};
+    if(suffix == "i32") return Literal{*from_sv<i32>(number)};
+    if(suffix == "i64") return Literal{*from_sv<i64>(number)};
+    if(suffix == "u8") return Literal{*from_sv<u8>(number)};
+    if(suffix == "u16") return Literal{*from_sv<u16>(number)};
+    if(suffix == "u32") return Literal{*from_sv<u32>(number)};
+    if(suffix == "u64") return Literal{*from_sv<u64>(number)};
+
+    return Literal{*from_sv<i32>(number)};
+  }
+  case TokenType::LiteralFloat:
+    return sv.back() == 'f' ? Literal{static_cast<f32>(atof(std::string(sv.begin(), sv.end() - 1).c_str()))}
+                            : Literal{atof(std::string(sv.begin(), sv.end()).c_str())};
+  case TokenType::LiteralBool: return Literal{sv[0] == 't'};
+  case TokenType::LiteralString: return Literal{std::string(sv.begin() + 1, sv.end() - 1)};
+  }
+}
+
+} // namespace ooze
